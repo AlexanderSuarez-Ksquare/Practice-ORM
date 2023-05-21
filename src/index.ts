a4ca5ea6f68d7e55129f89d28d859ebf08996037
Alexander Suarez <alexander.suarez@itksquare.edu.mx>
2023-05-21 14:34:50 -0600

    ORM done

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import app from "./app";
+import {startDB} from "./db"
+const PORT = 5000;
+dotenv.config();
+const URI_CONN = process.env.URI_CONN;
+
+app.listen(PORT,async ()=>{
+    try {
+        if(URI_CONN){
+            const sequelize = startDB(URI_CONN)
+            await sequelize.authenticate()
+            // await sequelize.sync({force:true})
+            await sequelize.sync({force:true})
+            console.log(`App is listening at port ${PORT}`)
+        }
+    } catch (error) {
+        console.error(error)
+        process.abort()
+    }
+})
